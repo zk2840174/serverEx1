6b841ec684715a3f7e06ec49c4c0bc642b73f15b
zerock <zk2020308a@gmail.com>
2024-11-22 20:15:14 +0900

    Hello Commit

--- src/main/java/org/zerock/serverex1/controller/HelloController.java
+++ src/main/java/org/zerock/serverex1/controller/HelloController.java
@@ -1,4 +1,20 @@
 package org.zerock.serverex1.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sample")
 public class HelloController {
+
+    @GetMapping("hello")
+    public String[] hello() {
+
+        long time = System.currentTimeMillis();
+
+        return new String[] { "Hello World", Long.toString(time) };
+    }
+
 }
